c8f010a1212f0adefd816f44249f42b4d06df966
gurusharan <gurusharan200@gmail.com>
2017-03-04 19:29:07 -0500

    username password authentication callback

--- models/user.js
+++ models/user.js
@@ -23,3 +23,29 @@ var UserSchema = mongoose.Schema({
 
 var User = module.exports = mongoose.model('User', UserSchema);
 
+module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+    var query = {username: username}
+    User.findOne(query, callback);
+}
+
+module.exports.addUser = function(newUser, callback){
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+
+        });
+    });
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) =>{
+        if(err) throw err;
+        callback(null, isMatch);
+    });
+}
