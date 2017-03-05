9ede21aa1366d698d5fb11955748180e3d35541d
gurusharan <gurusharan200@gmail.com>
2017-03-04 19:18:11 -0500

    Backend api

---
+++ app.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var passport = require('passport');
+var mongoose = require('mongoose');
+var config = require('./config/database');
+
+//Connect to Database
+mongoose.connect(config.database);
+
+//On Connection
+mongoose.connection.on('connected', () => {
+    console.log('connected to database'+ config.database);
+});
+
+//On Error
+mongoose.connection.on('error', (err) => {
+    console.log('database error'+ err);
+});
+
+//Initializing express
+var app = express();
+
+//Accessing users from routes folder
+var users = require('./routes/users');
+ 
+//Port Number
+var port = 3000;
+
+//CORS Middleware
+app.use(cors());
+
+//Set Static Folder for Angular
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+//Body parser Middleware
+app.use(bodyParser.json());
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+//path for user routes
+app.use('/users', users);
+
+//Index Route
+app.get('/',(req, res)=>{
+    res.send('Invalid endpoint');
+});
+
+//For all other routes other than defined
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+//Start Server
+app.listen(port, function(req, res){
+    console.log('server started on port' + port);
+})
