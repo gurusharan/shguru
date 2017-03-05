3f479523c9312bc4e9bf486a257e2bf5b6db6653
gurusharan <gurusharan200@gmail.com>
2017-03-04 19:26:16 -0500

    register authenticate end points updated for response

--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,14 @@ router.post('/register',(req, res, next)=>{
         password: req.body.password
     });
 
+  User.addUser(newUser, (err, user) => {
+    if(err){
+        res.json({success: false, msg:'failed to register user'});
+    }
+    else{
+        res.json({success: true, msg:'User registered'});
+    }
+  });
 });
 
 //Authenticate
@@ -21,7 +29,34 @@ router.post('/authenticate',(req, res, next)=>{
     var username = req.body.username;
     var password = req.body.password;
 
+    User.getUserByUsername(username, (err, user)=>{
+        if(err) throw error;
+        if(!user){
+            return res.json({success: false, msg:'User not found'});
+        }
 
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if(err) throw err;
+            if(isMatch){
+                var token = jwt.sign(user, config.secret, {
+                    expiresIn: 604800 // seconds in a week 
+                });
+
+                res.json({
+                    success: true,
+                    token: 'JWT '+ token,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email
+                    }
+                });
+            } else {
+                return res.json({success: false, msg:'Wrong password'});
+            }
+        }); 
+    });
 });
 
 //Profile
