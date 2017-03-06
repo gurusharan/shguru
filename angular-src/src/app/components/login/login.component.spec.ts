9231b89c65e0ca249d659dd3c0a17b6da1a57e93
gurusharan <gurusharan200@gmail.com>
2017-03-05 23:08:54 -0500

    few tests for login component checking

--- angular-src/src/app/components/login/login.component.spec.ts
+++ angular-src/src/app/components/login/login.component.spec.ts
@@ -8,19 +8,25 @@ import { LoginComponent } from './login.component';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ]; //declare the test component
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
+    component = fixture.componentInstance; //LoginComponent test instance
     fixture.detectChanges();
   });
+
+  //Query for the title <h2> by CSS element selector
+  de = fixture.debugElement.query(By.css('h1'));
+  el = de.nativeElement;
 
   it('should create', () => {
     expect(component).toBeTruthy();
